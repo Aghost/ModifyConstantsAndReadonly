c24e5184ddedb2801ba7ba28f1236a39d4dd1d41
Aghost <youri_van_sorgen2@example.com>
2021-09-15 11:22:16 +0200

    initial

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using static System.Console;
+
+namespace ModifyConstantsAndReadOnly
+{
+    class ROExample {
+        public readonly string readoString = "I cannot be changed";
+    }
+
+    class Program
+    {
+        public const string constString = "I cannot be changed";
+
+		static unsafe void Main (string[] args) {
+            // modify const strings
+            WriteLine(constString);
+            StringManipulation(constString, "I can be changed   ");
+            WriteLine(constString);
+
+            ROExample rx = new();
+            WriteLine(rx.readoString);
+
+            // modify readonly strings
+            StringManipulation(rx.readoString, "I can be changed   ");
+            WriteLine(rx.readoString);
+		}
+
+        static unsafe void StringManipulation(string str, string to_this) {
+            fixed(char* ps = str)
+                for (int i = 0; i < str.Length; i++) {
+                    ps[i] = to_this[i];
+                }
+
+            WriteLine(str);
+        }
+
+	}
+}
